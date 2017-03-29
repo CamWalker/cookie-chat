7856f158fbecde5293dd193eaca632fdfa29d616
Cameron <Cameron@user1s-MacBook-Pro.local>
2017-03-29 16:19:54 -0600

    done

--- js/messageService.js
+++ js/messageService.js
@@ -5,15 +5,58 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
+  this.getMessages = function () {
+    var promise = $http({
+      method: 'GET',
+      url: 'https://practiceapi.devmounta.in/api/chats'
+    });
+    // console.log('prmise',promise);
 
+    return promise.then(function(response) {
+      console.log('resp',response);
+      return response.data;
+    })
+  }
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
+  this.postMessage = function (newMessage) {
+    return promise = $http({
+      method: 'POST',
+      url: 'https://practiceapi.devmounta.in/api/chats',
+      data: {
+        message: newMessage
+      }
+    }).then(function (response) {
+      if (response.status !== 200) {
+        alert('Message failed to send')
+      } else {
+        return response.data;
+      }
+
+    })
+  }
 
 
+  this.deleteMessage = function(message) {
+    $http({
+      method: 'PUT',
+      url: 'https://practiceapi.devmounta.in/api/chats',
+      data: {
+        message: message
+      }
+    }).then(function (response) {
+      if (response.status !== 200) {
+        alert('Message failed to delete')
+      } else {
+        console.log(response.data);
+      }
+
+    })
+  }
 
 
 });
